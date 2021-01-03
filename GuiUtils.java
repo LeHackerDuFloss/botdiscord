66b1074c66cec6993b1c22920010e62f0b5f6625
Skylined-dev <54574881+Skylined-dev@users.noreply.github.com>
2021-01-03 13:30:49 +0100

    Add files via upload

---
+++ GuiUtils.java
@@ -0,0 +1,75 @@
+package fr.skylined.shopgui.utils;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class GuiUtils{
+	
+	public static void addItem(Material block, List<String> lore, int quantity, Inventory inv, String name, int place) {
+		ItemStack item = new ItemStack(block, quantity);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName(name);
+		itemMeta.setLore(lore);
+		item.setItemMeta(itemMeta);
+		inv.setItem(place, item);
+	}
+	
+	public static void addItem(Material block, List<String> lore, int quantity, Inventory inv, int place) {
+		ItemStack item = new ItemStack(block, quantity);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName("§0");
+		itemMeta.setLore(lore);
+		item.setItemMeta(itemMeta);
+		inv.setItem(place, item);
+	}
+	
+	public static void addItem(Material block, List<String> lore, Inventory inv, String name, int place) {
+		ItemStack item = new ItemStack(block, 1);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName(name);
+		itemMeta.setLore(lore);
+		item.setItemMeta(itemMeta);
+		inv.setItem(place, item);
+	}
+	
+	public static void addItem(Material block, List<String> lore, Inventory inv, int place) {
+		ItemStack item = new ItemStack(block, 1);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName("§0");
+		itemMeta.setLore(lore);
+		item.setItemMeta(itemMeta);
+		inv.setItem(place, item);
+	}
+	
+	public static void fillItem(Material block, List<String> lore, Inventory inv) {
+		ItemStack item = new ItemStack(block, 1);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setDisplayName("§0");
+		itemMeta.setLore(lore);
+		item.setItemMeta(itemMeta);
+		for(int slot = 0; slot < inv.getSize(); slot++) {
+			if(inv.getItem(slot) == null) {
+				inv.setItem(slot, item);
+			}
+		}
+		
+		
+		
+	}
+	
+	public static Inventory addGui(String name, int size) {
+		Inventory inv = Bukkit.createInventory(null, size, name);
+		return inv;
+	}
+	
+	public static void openGui(Player player, Inventory inv) {
+		player.openInventory(inv);
+	}
+
+}
